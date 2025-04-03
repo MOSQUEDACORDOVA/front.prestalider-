2ac5deaf72504455ab0b3ec21ddebcb5027df4f1
andresaoa <andresrendon@aoacolombia.com>
2025-04-03 11:01:55 -0500

    module clients

--- src/views/client-page/client-page.js
+++ src/views/client-page/client-page.js
@@ -1,3 +1,21 @@
+// tu-view-model.js
 export class ClientPage {
-    // Class methods and properties here
- }
+    createModal;
+    referenceModal;
+  
+    attached() {
+      this.setupModalHandlers();
+    }
+  
+    setupModalHandlers() {
+      const addReferenceButton = this.createModal.querySelector('[data-bs-target="#referenceModal"]');
+      
+      if (addReferenceButton) {
+        addReferenceButton.addEventListener('click', (e) => {
+          e.preventDefault();
+          const referenceModalInstance = new bootstrap.Modal(this.referenceModal);
+          referenceModalInstance.show();
+        });
+      }
+    }
+  }
