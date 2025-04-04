8f46ab37c296521f2e14b91dee0add9ad1db1440
andresaoa <andresrendon@aoacolombia.com>
2025-04-03 20:19:17 -0500

    fix error clients

--- src/views/client-page/client-page.js
+++ src/views/client-page/client-page.js
@@ -1,21 +1,4 @@
 // tu-view-model.js
 export class ClientPage {
-    createModal;
-    referenceModal;
   
-    attached() {
-      this.setupModalHandlers();
-    }
-  
-    setupModalHandlers() {
-      const addReferenceButton = this.createModal.querySelector('[data-bs-target="#referenceModal"]');
-      
-      if (addReferenceButton) {
-        addReferenceButton.addEventListener('click', (e) => {
-          e.preventDefault();
-          const referenceModalInstance = new bootstrap.Modal(this.referenceModal);
-          referenceModalInstance.show();
-        });
-      }
-    }
   }
